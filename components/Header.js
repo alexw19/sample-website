3f5de1e058032c7bf61b68b7632af00b0de99b71
Alex Wang <alex.wang@example.com>
2023-05-17 16:51:00 -0500

    update header

--- components/Header.js
+++ components/Header.js
@@ -1,8 +1,16 @@
 // import { Dropdown } from "bootstrap";
+import { useState } from "react";
 import React from "react";
 import Dropdown from './Dropdown'
+// import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Header = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleNavbar = () => {
+        setIsOpen(!isOpen);
+    };
+
     return (
         <section class="common-header">
             <div class="container p-0">
@@ -11,12 +19,11 @@ const Header = () => {
                         <a class="navbar-brand" href="#">
                             <img src="/logo-01.png" class="img-fluid logo width-40" alt="logo"/>
                         </a>
-                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                            data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false"
-                            aria-label="Toggle navigation">
+                        <button class="navbar-toggler" onClick={toggleNavbar} type="button" data-bs-toggle="collapse"
+                            data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown">
                             <span class="navbar-toggler-icon"></span>
                         </button>
-                        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                        <div className={isOpen ? "" : "collapse navbar-collapse"} id="navbarNavDropdown">
                             <ul class="navbar-nav">
                                 <li class="nav-item">
                                     <a class="nav-link" aria-current="page" href="/">Home</a>
@@ -27,9 +34,9 @@ const Header = () => {
                                 <li class="nav-item dropdown">
                                     <Dropdown/>
                                 </li>
-                                <li class="nav-item">
+                                {/* <li class="nav-item">
                                     <a class="nav-link" href="/blogs">Blog</a>
-                                </li>
+                                </li> */}
                             </ul>
                             <div class="">
                                 <a class="contactus-btn btn" href="/contact">Contact Us</a>
