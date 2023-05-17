715e4f980eb34f81b36a94aff255872b239956a7
Alex <alex.wang19@gmail.com>
2023-05-17 09:17:14 -0500

    Update Dropdown.js

--- components/Dropdown.js
+++ components/Dropdown.js
@@ -3,20 +3,20 @@ import styles from "./Dropdown.module.css";
 
 const Dropdown = () => {
   return (
-    <div className={styles.dropdown}>
-      <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink"
-        data-bs-toggle="dropdown" aria-expanded="false">
-        Solutions
-      </a>
-      <div className={styles.dropdownContent} aria-labelledby="navbarDropdownMenuLink">
-        {/* <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink"> */}
-          <a class="dropdown-item" href="digital_transformation">Digital Transformation</a>
-          <a class="dropdown-item" href="cyber_security">Cyber Security</a>
-          <a class="dropdown-item" href="portfolio_management">Portfolio Management</a>
-          <a class="dropdown-item" href="resource_staffing">Resource & Staffing</a>
-        {/* </ul> */}
+      <div className={styles.dropdown}>
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink"
+          data-bs-toggle="dropdown" aria-expanded="false">
+          Solutions
+        </a>
+        <div className={styles.dropdownContent} aria-labelledby="navbarDropdownMenuLink">
+          {/* <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink"> */}
+            <a class="dropdown-item" href="digital_transformation">Digital Transformation</a>
+            {/* <a class="dropdown-item" href="cyber_security">Cyber Security</a> */}
+            <a class="dropdown-item" href="portfolio_management">Portfolio Management</a>
+            <a class="dropdown-item" href="resource_staffing">Resource & Staffing</a>
+          {/* </ul> */}
+        </div>
       </div>
-    </div>
   );
 };
 
